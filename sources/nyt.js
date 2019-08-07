a084aeb6cddf8fb6d2009b98c15714a03f3a6e14
Kevin Briggs <kevinbriggs1@gmail.com>
2019-08-07 08:06:26 -0400

    nyt going by section

--- sources/nyt.js
+++ sources/nyt.js
@@ -1,7 +1,8 @@
-const cheerio = require('cheerio')
+const cheerio= require('cheerio')
 const request = require('request');
 
-
+const subArticleClass = "css-i435f0"
+const sectionClass = "css-u82chm"
 
 const getNYT = new Promise((resolve, reject)=> {
     request({
@@ -11,19 +12,33 @@ const getNYT = new Promise((resolve, reject)=> {
         let nytimesArticles = [];
         if (err) return reject(err);
         let $ = cheerio.load(body);
-        $('a').each(function(i, el) {
-            const element = cheerio.load(el)
-            link = element('a').attr('href')
-            headline = element('a').text()
-            nytimesArticles.push({
-                headline, link
-            })
-    
+ 
+        $(`.${sectionClass}`).each((i, el)=> {
+              const section = $(el).find('h2').first().text();
+              console.log(section)
         })
+
+      
+        // const url = $(el).find('a').attr("href") 
+
+        // const section = url.split('/')[4]
+        // nytimesArticles.push({url, section})
+
+
+
+        // $('a').each(function(i, el) {
+        //     const element = cheerio.load(el)
+        //     link = element('a').attr('href')
+        //     headline = element('a').text()
+        //     nytimesArticles.push({
+        //         headline, link
+        //     })
     
-        nytimesArticles = nytimesArticles.filter((item)=> {
-            return (item.link && item.headline && (item.link[0] =='/'))
-        })
+        // })
+    
+        // nytimesArticles = nytimesArticles.filter((item)=> {
+        //     return (item.link && item.headline && (item.link[0] =='/'))
+        // })
     
         resolve(nytimesArticles);
     });
